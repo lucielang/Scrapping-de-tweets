4655e9a06302e8052c72281e468d9f1b7e0d41c1
lulucode <lucie.lang@ensae.fr>
2024-12-25 22:59:06 +0000

    mod

--- test_2.py
+++ test_2.py
@@ -1,5 +1,9 @@
-import nltk
-from nltk.probability import FreqDist
+import pandas as pd
 
-a = ["Je","tu","elle","il","on","nous","vous","ils","elles","vous","ils","nous"]
-fdist = FreqDist(a)
+# Charger le fichier CSV
+csv_file = '/home/onyxia/work/Scrapping_tweets/Scrapping_tweets/labeled_data_with_predictions.csv'  # Remplacez par le chemin de votre fichier CSV
+df = pd.read_csv(csv_file)
+
+# Sauvegarder le fichier en format Excel (XLSX)
+xlsx_file = '/home/onyxia/work/Scrapping_tweets/Scrapping_tweets/data_predictions.xlsx'  # Nom du fichier Excel de sortie
+df.to_excel(xlsx_file, index=False)  # index=False pour ne pas inclure l'index dans le fichier Excel
